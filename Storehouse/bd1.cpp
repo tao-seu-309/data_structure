011863eac8102059e3ee7d8a413493f6ca893ed8
tao-seu-309 <80880826+tao-seu-309@users.noreply.github.com>
2022-08-22 11:47:01 +0800

    Update bd1.cpp

--- Storehouse/bd1.cpp
+++ Storehouse/bd1.cpp
@@ -5,9 +5,6 @@ using namespace std;
 
 // write your code in C++ (C++14 (g++ 6.2.0))
 
-// int ans = 0;
-
-//
 int ans = 1;
 bool dfs(vector<vector<char>> &maze, vector<vector<int>> &visited, int i, int j, int m, int n) {
     if(i<0 || i>=m || j<0 || j>=n) {    //碰到边
@@ -22,9 +19,7 @@ bool dfs(vector<vector<char>> &maze, vector<vector<int>> &visited, int i, int j,
 
     if(visited[i][j] == 1) return 0;    //已经走过的路
     if(visited[i][j] == 2) return 0;    //传送带
-     
-    
-   
+            
     bool res;
     switch(maze[i][j]){
         case '.':
@@ -56,21 +51,22 @@ bool dfs(vector<vector<char>> &maze, vector<vector<int>> &visited, int i, int j,
         default:
         res = 0;
         break;
-    }
+    }        
     
-    visited[i][j] = 0;
-    // cout<<i<<"\t"<<j<<"\t"<<int(res)<<"\t";
-    if(res){                    //走的通的路标记为‘O’
+    if(res){                     //走的通的路标记为‘O’
         maze[i][j] = 'O';
-        return 1;
-    }else if(maze[i][j]!='.'){  //走不通的路标记为‘N’,                          
-        maze[i][j] = 'N';       //res==0的时候如果是在‘.’上不能判定走不通，因为实际情况可以往回走
         ans++;
-        cout<<ans<<endl;
+        visited[i][j] = 0;
+        cout<<i<<"\t"<<j<<"\t"<<int(res)<<"\t"<<ans<<endl;
+        return 1;
+    }else if(visited[i][j]==2){  //走不通的路标记为‘N’,                          
+        maze[i][j] = 'N';        //res==0的时候如果是在‘.’上不能判定走不通，因为实际情况可以往回走  
+        // visited[i][j] = 0;     
+        return 0;
+    }else{
+        visited[i][j] = 0;
         return 0;
     }
-
-    // return 0;
 
 }
 
@@ -86,8 +82,12 @@ int main(){
                                  {'.', 'U', '.', 'D', 'R'},
                                  {'.', 'U', 'L', 'L', '.'},
                                  {'.', '.', '.', '.', 'O'}};
+    // int m = 3, n = 4;
+    // vector<vector<char>> maze = {{'O', 'U', '.', '.'},
+    //                              {'L', '.', '.', '.'},                           
+    //                              {'.', '.', '.', '.'}};
 
-    maze[m-1][n-1] = 'O';
+    // maze[m-1][n-1] = 'O';
     vector<vector<int>> visited(m, vector<int>(n,0));
     for(int i=0; i<m; i++){
         for(int j=0; j<n; j++){
@@ -97,6 +97,6 @@ int main(){
         }
     }
 
-    cout << ans<< endl;
+    cout << m*n-ans<< endl;
     return 0;
 }
